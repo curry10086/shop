1d3c72770e61aac6f5f085bbe144c29077e87efc
curry10086 <614799842@qq.com>
2021-09-14 15:54:08 +0800

    完成用户列表的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,150 @@
 <template>
-  <div>
-    <el-button type="info" @click="exit">退出</el-button>
-  </div>
+  <el-container class="home-container">
+      <!-- 头部区域 -->
+    <el-header id="el_header">
+        <div>
+            <img src="../assets/heima.png" alt="">
+            <span class="head_title">电商管理平台</span>
+        </div>
+        <el-button type="info" @click="exit">退出</el-button>
+        
+    </el-header>
+    <!-- 页面主体 -->
+    <el-container>
+        <!-- 侧边栏 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+            <!-- 侧边栏菜单区 -->
+            <div class="toggle-button" @click="toggleCollapse">|||</div>
+            <el-menu
+                background-color="#333744"
+                text-color="#fff"
+                active-text-color="#409EFF"
+                :unique-opened = 'true'
+                :collapse = "isCollapse"
+                :collapse-transition = "false"
+                :router = "true"
+                :default-active = "activePath">
+                <!-- 一级菜单 -->
+            <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+                <!-- 一级菜单模板区 -->
+                <template slot="title">
+                    <!-- 图标 -->
+                    <i :class="iconsObj[item.id]"></i>
+                    <!-- 文本 -->
+                    <span>{{item.authName}}</span>
+                </template>
+                <!-- 二级菜单 -->
+                <el-menu-item :index="'/' + childItem.path + ''" v-for="childItem in item.children" :key="childItem.id"
+                 @click="saveActivePath('/' + childItem.path)">
+                    <template slot="title">
+                    <!-- 图标 -->
+                    <i class="el-icon-menu"></i>
+                    <!-- 文本 -->
+                    <span>{{childItem.authName}}</span>
+                </template>
+                </el-menu-item>
+            </el-submenu>
+           
+            </el-menu>
+    </el-aside>
+      <!-- 右侧主体 -->
+      <el-main>
+          <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+      return {
+          menuList:[],
+          iconsObj:{
+              '125':'iconfont icon-user',
+              '103':'iconfont icon-tijikongjian',
+              '101':'iconfont icon-shangpin',
+              '102':'iconfont icon-danju',
+              '145':'iconfont icon-baobiao'
+          },
+        //   是否折叠
+          isCollapse:false,
+        //   动态绑定状态
+          activePath:''
+      }
+  },
+  created() {
+      this.getMenuList(),
+      this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
     exit() {
       window.sessionStorage.clear();
       this.$router.push("/login");
+    },
+    // 获取所有菜单
+    async getMenuList(){
+        const {data:res} =  await this.$http.get('menus')
+        if(res.meta.status !== 200) return this.$message.error(res.meta.msg)
+        this.menuList = res.data
+        console.log(res)
+        
+    },
+    // 点击,侧边栏折叠与展开
+    toggleCollapse(){
+        this.isCollapse = !this.isCollapse
+    },
+    // 保存状态
+    saveActivePath(activePath){
+        window.sessionStorage.setItem("activePath",activePath)
+        this.activePath = activePath;
+        
     },
   },
 };
 </script>
 
 <style>
+#el_header{
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+  
+}
+#el_header div{
+    display: flex;
+    align-items: center;
+}
+#el_header div span{
+    margin-left: 15px;
+}
+.el-aside{
+    background-color: #333744;
+}
+.el-aside .el-menu{
+    border-right: 0;
+}
+.el-main{
+    background-color: #eaedf1;
+}
+.home-container{
+    height: 100%;
+}
+.iconfont{
+    margin-right: 10px;
+}
+.toggle-button{
+    background-color: #4a5064;
+    color: #fff;
+    font-size: 10px;
+    text-align: center;
+    line-height: 24px;
+    letter-spacing: 0.2em;
+    cursor: pointer;
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import VueRouter from 'vue-router'
 
 import Login from '../components/Login'
 import Home from '../components/Home'
+import Welcome from '../components/Welcome'
+import User from '../components/user/User'
+
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,7 +20,19 @@ const routes = [
   },
   {
     path: '/home',
-    component: Home
+    component: Home,
+    redirect:'/welcome',
+    children:[{
+      path:'/welcome',
+      component:Welcome  
+    },
+    {
+      path: '/users',
+      component: User
+    }
+
+
+    ]
   }
  
 ]
